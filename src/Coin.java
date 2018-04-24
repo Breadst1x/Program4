6f7bba7e104453150ea46b484843569af07114f1
Smileefacee <keetonhersey@gmail.com>
2018-04-24 16:55:51 -0600

    Added Algorithm

--- src/Coin.java
+++ src/Coin.java
@@ -1,52 +1,49 @@
-
 /*
- * Author: Anthony Cordero
- * Date: 4/13/18
- * Overview: algorithm to make the fewest amount of coins for change
+ * Authors: Keeton Hersey, AJ Cordero
+ * Date: 4.20.18 lab 7
+ * Notes: This lab was to implement the usage of JTests and the parsing of coin values
  */
+
+
 
 import java.util.Arrays;
 
 public class Coin {
-	public static int[] coinArray(int[] values, int centValue) {
-		int[] output = new int[centValue];//creates output array
-		int step = 0;//stepper
-		Arrays.sort(values);//sorts the values of the array
-		while(centValue >= values[0])
-			//while the value of the coin is higher than the change value
-			
+	public static int[] coinArray(int[] coinValues, int centValue) {
+		int[] out = new int[centValue];
+		int iter = 0;
+		Arrays.sort(coinValues);
+		while(centValue >= coinValues[0])
 		{
-			for(int i = values.length-1; i >= 0; i--)//for each item in the value array, this runs
+			for(int i = coinValues.length-1; i >= 0; i--)
 			{
-				if(centValue >= values[i])//if the value of the coin is greater than the value needed for change,
+				if(centValue >= coinValues[i])
 				{
-					output[step] = values[i];//the current value of the iterator is set to the value in the array
-					centValue -= values[i];//decreases the value of the change needed to be dispensed
+					out[iter] = coinValues[i];
+					centValue -= coinValues[i];
 					break;
 				}
 			}
-			step++;//increment
+			iter++;
 		}
-		for(int i = 0; i < output.length; i++)
-			//for each item in the output array,
+		for(int i = 0; i < out.length; i++)
 		{
-			if(output[i] == 0)//if the output is 0
-			{//there is no need to carry on. This is the end of the array.
+			if(out[i] == 0)
+			{
 				if(i == 0)
 					break;
-				int[] newOut = new int[i];//new array
+				int[] reOut = new int[i];
 				for(int j = 0; j < i; j++)
 				{
-					newOut[j] = output[j];//creates a temporary name place holder
+					reOut[j] = out[j];
 				}
-				output = newOut;
+				out = reOut;
 				break;
 			}
 		}
-		return output;
+		return out;
 	}
-	//returns the number of coins
-	public static int coinAdder(int[] in)
+	public static int sumLength(int[] in)
 	{
 		return in.length;
 	}
